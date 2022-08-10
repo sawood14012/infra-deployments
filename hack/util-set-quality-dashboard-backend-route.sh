7d61aa8048d90ac9d5e10afde96b141880fc00d8
Meer Sawood <msawood@redhat.com>
2022-08-10 18:50:34 +0530

    try

--- hack/util-set-quality-dashboard-backend-route.sh
+++ hack/util-set-quality-dashboard-backend-route.sh
@@ -4,4 +4,5 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 KUSTOMIZATION_FILE="$ROOT/components/quality-dashboard/frontend/kustomization.yaml"
 CLUSTER_URL_HOST=$(oc whoami --show-console|sed 's|https://console-openshift-console.apps.||')
 
-sed -i "s|appstudio-stage.x99m.p1.openshiftapps.com|${CLUSTER_URL_HOST}|" "$KUSTOMIZATION_FILE"
+#yq e -i "s|appstudio-stage.x99m.p1.openshiftapps.com|${CLUSTER_URL_HOST}|" "$KUSTOMIZATION_FILE"
+yq e -i "(.configMapGenerator[].literals[] | select(. == \"appstudio-stage.x99m.p1.openshiftapps.com\")) = \"$CLUSTER_URL_HOST\"" "$KUSTOMIZATION_FILE"
